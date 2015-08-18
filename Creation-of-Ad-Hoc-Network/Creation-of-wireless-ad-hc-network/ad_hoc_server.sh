b9cb4e459d0cf8c167c2638014057642cf2d0ea7
Hrishikesh <hrishikeshtak@gmail.com>
2015-08-18 22:54:03 +0530

    Creation of wireless Ad-Hoc network

---
+++ Creation-of-Ad-Hoc-Network/Creation-of-wireless-ad-hc-network/ad_hoc_server.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+## Shell Script For Server
+## For creating Ad-hoc Wireless network
+
+clear
+echo "Script For Wireless Ad-hoc network"
+sudo apt-get install ssh -y
+sudo sed -ie '$a#Wireless Ad-hoc network Setup' /etc/network/interfaces
+sudo sed -ie '$aauto wlan0' /etc/network/interfaces
+sudo sed -ie '$aiface wlan0 inet static' /etc/network/interfaces
+sudo sed -ie '$aaddress 192.168.1.90' /etc/network/interfaces
+sudo sed -ie '$anetmask 255.255.255.0' /etc/network/interfaces
+sudo sed -ie '$awireless-channel 1' /etc/network/interfaces
+sudo sed -ie '$awireless-essid MYNETWORK' /etc/network/interfaces
+sudo sed -ie '$awireless-mode ad-hoc' /etc/network/interfaces
+
+sudo service network-manager restart	
+sudo ifdown wlan0
+sudo ifup wlan0
+echo "Done !!! "
+
+exit 0
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Faster-Map-Query-Resolution-using-Hadoop-based-Spark-Cluster
 This is my M-Tech Project , this project contains the Map query resolutions using Spark cluster .
+
+
