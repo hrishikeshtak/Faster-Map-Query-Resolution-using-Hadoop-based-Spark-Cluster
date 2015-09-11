efb974e6106c2d2c7a51f0a9c43025d44163f623
Hrishikesh <hrishikeshtak@gmail.com>
2015-09-11 20:57:06 +0530

    Hadoop Wordcount

---
+++ Hadoop-wordcount-example/README.md
@@ -0,0 +1,3 @@
+Run Hadoop Hello World Example ( Wordcount )
+
+First check that your Hadoop Cluster is Ready or Not.
---
+++ Hadoop-wordcount-example/run_hadoop-2.6.0_wordcount_program.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear;
+echo "Running Hadoop Wordcount Example ... "
+if [ $# -eq 0 ]
+then
+		echo "\n\nEnter input File name : ";
+		exit 1;
+fi
+
+sudo -u hduser /usr/local/hadoop/bin/hdfs dfs -ls /;
+sudo -u hduser /usr/local/hadoop/bin/hdfs dfs -copyFromLocal $1 /file;
+sudo -u hduser /usr/local/hadoop/bin/yarn jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.6.0.jar wordcount /file /out;
+clear;
+echo "\n\nOutput of Wordcount : \n";
+sudo -u hduser /usr/local/hadoop/bin/hdfs dfs -cat /out/part*
