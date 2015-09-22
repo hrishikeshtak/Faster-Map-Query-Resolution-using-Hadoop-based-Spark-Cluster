888262a871dab66e6ff179f145ed6cc1d2309d1f
Hrishikesh <hrishikeshtak@gmail.com>
2015-09-22 20:38:59 +0530

    Hadoop wordcount Example

--- Hadoop-wordcount-example/README.md
+++ Hadoop-wordcount-example/README.md
@@ -1,3 +1,8 @@
-Run Hadoop Hello World Example ( Wordcount )
+## Run Hadoop Hello World Example ( Wordcount )
 
 First check that your Hadoop Cluster is Ready or Not.
+
+#### How to run script
+
+./run_hadoop-2.2.0_wordcount_program.sh FILE_NAME
+
--- Hadoop-wordcount-example/run_hadoop-2.2.0_wordcount_program.sh
+++ Hadoop-wordcount-example/run_hadoop-2.2.0_wordcount_program.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-
 clear;
-echo "Running Hadoop Wordcount Example ... "
+echo -e "Running Hadoop Wordcount Example ... "
+####################################################################################################
+INPUT_FILE=$1;
+print_usage() {
+		echo -e "\nUsage: $0 <INPUT_FILE>"
+		echo -e "    INPUT_FILE - Specify Input File Name "
+}
 if [ $# -eq 0 ]
 then
-		echo "\n\nEnter input File name : ";
-		exit 1;
+		print_usage;
+		exit 0;
 fi
 ####################################################################################################
 sudo -u hduser /usr/local/hadoop/bin/hdfs dfs -ls /;
-sudo -u hduser /usr/local/hadoop/bin/hdfs dfs -copyFromLocal $1 /file;
+sudo -u hduser /usr/local/hadoop/bin/hdfs dfs -copyFromLocal $INPUT_FILE /file;
 sudo -u hduser /usr/local/hadoop/bin/yarn jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.2.0.jar wordcount /file /out;
 ####################################################################################################
 clear;
-echo "\n\nOutput of Wordcount : \n";
+echo -e "\n\nOutput of Wordcount : \n";
 sudo -u hduser /usr/local/hadoop/bin/hdfs dfs -cat /out/part*
 ####################################################################################################
